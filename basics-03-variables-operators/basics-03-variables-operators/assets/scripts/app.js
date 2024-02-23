431c588b6ee6d3fb5d3dd47b24fa0a05c55a7562
parkjise <monories79@gmail.com>
2024-02-23 10:09:41 +0900

    start

---
+++ basics-03-variables-operators/basics-03-variables-operators/assets/scripts/app.js
@@ -0,0 +1,66 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+
+//Gets input from input field
+function getUserNumberInput(){
+  return parseInt(userInput.value);
+}
+
+// Generates and writes calculation log
+function createAndWriteOutput(operator,resultBeforeCalc,calcNumber){
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult,calcDescription);// from vendor file
+}
+
+function add(){
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult += enteredNumber;
+  createAndWriteOutput('+',initialResult,enteredNumber);
+}
+
+function subtract(){
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult -= enteredNumber;
+  createAndWriteOutput('-',initialResult,enteredNumber);
+}
+
+function multiply(){
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNumber;
+  createAndWriteOutput('*',initialResult,enteredNumber);
+}
+
+function divide(){
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult /= enteredNumber;
+  createAndWriteOutput('/',initialResult,enteredNumber);
+}
+
+addBtn.addEventListener('click',add);
+subtractBtn.addEventListener('click',subtract);
+multiplyBtn.addEventListener('click',multiply);
+divideBtn.addEventListener('click',divide);
+
+// currentResult = add(15,23);
+// currentResult = (currentResult +  10) * 3 / 2 - 1;
+
+// let calculationDescription ='(' + currentResult + ' +  10) * 3 / 2 - 1;'
+// let errorMessage = 'An error \n' +  
+//                     'occurred !'
+
+
+
+
+// const defaultResult = 0;
+// let currentResult = defaultResult;
+
+// currentResult = (currentResult + 10) * 3 / 2 - 1;
+
+// let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
+
+// outputResult(currentResult, calculationDescription);
