8ba330737cef95f7aafa8c03663e4129e70fcf83
js.park1021 <js.park1021@ptn.tmoney.co.kr>
2024-11-01 11:29:16 +0900

    1101

--- Section6-Function/functions-04-game-user-choice/script.js
+++ Section6-Function/functions-04-game-user-choice/script.js
@@ -1,32 +1,58 @@
-const startGameBtn = document.getElementById('start-game-btn');
+const startGameBtn = document.getElementById("start-game-btn");
 
-const ROCK = 'ROCK';
-const PAPER = 'PAPER';
-const SCISSORS = 'SCISSORS';
-const DEFAULT_USER_CHOICE =ROCK;
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = "DRAW";
+const RESULT_PLAYER_WINS = "PLAYER_WINS";
+const RESULT_COMPUTER_WINS = "COMPUTER_WINS";
 
 let gameIsRunning = false;
 
-const getPlayerChoice = function(){
-  const selection = prompt(`${ROCK},${PAPER},${SCISSORS}`,'').toUpperCase();
+const getPlayerChoice = function () {
+  const selection = prompt(`${ROCK},${PAPER},${SCISSORS}`, "").toUpperCase();
 
-  if(
-    selection !== ROCK &&
-    selection !== PAPER &&
-    selection !== SCISSORS
-  ){
-    alert(`Invalid Choice ! We chose  ${DEFAULT_USER_CHOICE} for you!`)
-    return DEFAULT_USER_CHOICE
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid Choice ! We chose  ${DEFAULT_USER_CHOICE} for you!`);
+    return DEFAULT_USER_CHOICE;
   }
   return selection;
-}
+};
 
-startGameBtn.addEventListener('click',function(){
-  if(gameIsRunning){
-    return
+const getComputerChoice = function () {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+const getWinner = function (cChoice, pChoice) {
+  if (cChoice === pChoice) {
+    return RESULT_DRAW;
+  } else if (
+    (cChoice === ROCK && pChoice === PAPER) ||
+    (cChoice === PAPER && pChoice === SCISSORS) ||
+    (cChoice === SCISSORS && pChoice === ROCK)
+  ) {
+    return RESULT_PLAYER_WINS;
+  } else {
+    return RESULT_COMPUTER_WINS;
+  }
+};
+
+startGameBtn.addEventListener("click", function () {
+  if (gameIsRunning) {
+    return;
   }
   gameIsRunning = true;
-  console.log("Game is Starting...")
-  const playerSelection = getPlayerChoice();
-  console.log(playerSelection)
-})
+  console.log("Game is Starting...");
+  const playerChoice = getPlayerChoice();
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(computerChoice, playerChoice);
+  console.log(winner);
+});
